6a56f958ad7668451844494d779f3e871d5e0a36
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2023-10-02 23:23:49 +0530

    home controller and router

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+app.use("/", require("./routes"));
 
-app.listen(port, function (err) {
+app.listen(8000, function (err) {
   if (err) {
     console.log(`Error is running the server: ${err}`);
   }
